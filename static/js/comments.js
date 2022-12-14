b4422271bf78ea782f8826d4d91b287f5360fb93
Reinis <reinisgangis@gmail.com>
2022-12-15 00:51:57 +0200

    updated UI/UX

--- static/js/comments.js
+++ static/js/comments.js
@@ -1,3 +1,5 @@
+
+// Test function to select and display the comment with id
 function selectComment(id, comment) {
     console.log({"id": id, "comment": comment})
     var ele = document.getElementById("selected-comment")
