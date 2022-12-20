67c09b7a7f32c63a93cd362aa79de9277b65bad5
Niks Martinsons <66245053+nmartinsons@users.noreply.github.com>
2022-12-20 22:13:28 +0200

    last changes

--- static/js/greed_and_fear.js
+++ static/js/greed_and_fear.js
@@ -1,33 +1,36 @@
 
-  
   async function displayIndexOne() {
     let joke = await getGreedIndex();
     let target = document.getElementById('fng');
     target.innerText = fng.value;
   }
-
-  async function displayIndex(){
-    let response = await fetch("https://api.alternative.me/fng/");
-    let data = await response.json();
-    let target = document.getElementById('fng');
-    target.innerText = data.data[0].value;
-    return data.data[0].value;
+  
+  function giveIndex(index){
+    var data = [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: index,
+        title: { text: "Fear and Greed Index" },
+        type: "indicator",
+        mode: "gauge+number",
+        delta: { reference: 40 },
+        gauge: { axis: { range: [null, 100] } }
+      }
+    ];
+    
+    angular.element(document).ready(function () {
+      var layout = { width: 700, height: 400 };
+      Plotly.newPlot('fng', data, layout);
+    });
+    
   }
 
-  var data = [
-    {
-      domain: { x: [0, 1], y: [0, 1] },
-      value: 28,
-      title: { text: "Fear and Greed Index" },
-      type: "indicator",
-      mode: "gauge+number",
-      delta: { reference: 40 },
-      gauge: { axis: { range: [null, 100] } }
-    }
-  ];
-  
-  //console.log(data)
-  angular.element(document).ready(function () {
-    var layout = { width: 700, height: 400 };
-    Plotly.newPlot('fng', data, layout);
-});
+  function displayIndex() {
+    fetch("https://api.alternative.me/fng/")
+      .then(response => response.json())
+      .then(responseData => giveIndex(responseData.data[0].value))
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+  displayIndex()
